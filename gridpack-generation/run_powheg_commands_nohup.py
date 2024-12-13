3f54b8e490dd365dd8d4b758db170dbc6835412f
LukaLambrecht <Luka.Lambrecht@UGent.be>
2024-12-13 22:24:09 +0100

    add condor_q output check

--- gridpack-generation/run_powheg_commands_nohup.py
+++ gridpack-generation/run_powheg_commands_nohup.py
@@ -7,6 +7,7 @@
 
 # Note: the strategy is to use a python script that runs continuously in the background.
 # The problem is that it might get killed after some time on a shared environment (like lxplus or T2B).
+# This strategy is DEPRECATED and this script is not developed further, but kept as it is for reference.
 # See an alternative approach in run_powheg_commands_crontab.py!
 
 
@@ -16,14 +17,32 @@ import time
 import argparse
 
 
-def get_condor_q():
+def get_condor_q(do_check=False):
     # get the output by condor_q as a list of strings
     # note: works by writing condor_q output to a temporary file and reading it
+
+    # get the output of condor_q
     tempfile = 'check_running_jobs.txt'
     os.system('condor_q > {}'.format(tempfile))
     with open(tempfile, 'r') as f: lines = f.readlines()
     lines = [line for line in lines if line!='\n']
     os.system('rm {}'.format(tempfile))
+
+    # do a syntax check if requested
+    if do_check:
+        error = False
+        if len(lines) < 5: error = True
+        if not error:
+            if not lines[0].startswith('-- Schedd:'): error = True
+            if not lines[1].startswith('OWNER'): error = True
+            if not lines[-3].startswith('Total for query:'): error = True
+            if not lines[-2].startswith('Total for'): error = True
+            if not lines[-1].startswith('Total for all users:'): error = True
+        if error:
+            msg = 'WARNING: condor_q returned suspicious output:\n'
+            msg += '\n'.join(lines)
+            print(msg)
+            return None
     return lines
 
 def jobs_are_running():
@@ -66,12 +85,20 @@ def find_latest_jobid():
 def find_running_jobs(jobid):
     # find running jobs for a specific job id
 
-    lines = get_condor_q()
+    # get the output of condor_q command
+    # and return -1 if it looks like something is wrong
+    lines = get_condor_q(do_check=True)
+    if lines is None: return -1
+ 
+    # find line corresponding to given job id
     lines = [line for line in lines if line.split(' ')[2]==str(jobid)]
     if len(lines) == 0: return 0
     if len(lines) > 1:
-        msg = 'Ambiguity in job id, found lines {} for jobid {}'.format(lines, jobid)
-        raise Exception(msg)
+        msg = 'WARNING: ambiguity in job id, found multiple lines {} for jobid {}'.format(lines, jobid)
+        print(msg)
+        return -1
+
+    # split the line in parts and find the part corresponding to total number of jobs
     lineparts = [part for part in lines[0].split(' ') if part!='']
     ntotal = int(lineparts[8])
     return ntotal
