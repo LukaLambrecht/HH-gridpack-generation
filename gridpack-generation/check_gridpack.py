553a1f7ddfa1757f883e686bdd0132a964825a92
LukaLambrecht <Luka.Lambrecht@UGent.be>
2024-12-15 21:55:24 +0100

    check gridpack check

--- gridpack-generation/check_gridpack.py
+++ gridpack-generation/check_gridpack.py
@@ -16,6 +16,8 @@ if __name__=='__main__':
       help='Path to the gridpack to check')
     parser.add_argument('-n', '--nevents', default=10, type=int,
       help='Path to the gridpack to check')
+    parser.add_argument('--el7', default=False, action='store_true',
+      help='Run in el7 container')
     args = parser.parse_args()
     print('Running with following configuration:')
     for arg in vars(args): print('  - {}: {}'.format(arg, getattr(args,arg)))
@@ -25,16 +27,31 @@ if __name__=='__main__':
         msg = 'Provided gridpack {} does not exist.'.format(args.gridpack)
         raise Exception(msg)
 
+    # set path to this directory and tools directory
+    # (for use later)
+    thisdir = os.path.abspath(os.path.dirname(__file__))
+    toolsdir = os.path.abspath(os.path.join(thisdir, '../tools'))
+
     # make executable script
-    shfile = 'check_gridpack.sh'
-    with open(shfile, 'w'):
-        shfile.write('mkdir -p $TMPDIR/llambrec/\n')
-        shfile.write('cd $TMPDIR/llambrec/\n')
-        shfile.write('cp {} ./gridpack.tgz\n')
-        shfile.write('tar -xf ./gridpack.tgz\n')
-        shfile.write('./gridpack/\n')
-        shfile.write('./runcmsgrid.sh {} 1 1\n'.format(args.nevents))
+    exe = 'exe_check_gridpack.sh'
+    with open(exe, 'w') as f:
+        f.write('#!/bin/bash\n\n')
+        f.write('mkdir -p /tmp/$USER/\n')
+        f.write('cd /tmp/$USER/\n')
+        f.write('cp {} ./gridpack.tgz\n'.format(args.gridpack))
+        f.write('tar -xf ./gridpack.tgz\n')
+        f.write('./runcmsgrid.sh {} 1 1\n'.format(args.nevents))
+        f.write('cp cmsgrid_final.lhe {}'.format(thisdir))
+    os.system('chmod +x {}'.format(exe))
+    exe = os.path.abspath(exe)
+    cmd = 'bash {}'.format(exe)
+
+    # make el7 wrapping if requested
+    if args.el7:
+        thisdir = os.path.abspath(os.path.dirname(__file__))
+        toolsdir = os.path.abspath(os.path.join(thisdir, '../tools'))
+        run_in_container_script = os.path.join(toolsdir, 'run_in_el7_container.sh')
+        cmd = 'bash {} {}'.format(run_in_container_script, exe)
 
     # run the script in a job
-    cmd = 'bash {}'.format(shfile)
-    ct.submitCommandAsCondorJob('cjob_check_gridpack', cmd)
+    ct.submitCommandAsCondorJob('cjob_check_gridpack', cmd, jobflavour='workday')
