c2fe052f85adb810609a933e29f8b7a971fedc46
LukaLambrecht <Luka.Lambrecht@UGent.be>
2024-12-17 16:16:30 +0100

    update genproductions installation scripts

--- install-genproductions.sh
+++ install-genproductions.sh
@@ -2,10 +2,19 @@
 
 # Install the genproductions repository inside a suitable CMSSW release
 
+
+# get current working directory for later use
+cwd=$(pwd)
+
+# install CMSSW
 echo "Installing CMSSW_10_6_8..."
 cmsrel CMSSW_10_6_8
 cd CMSSW_10_6_8/src/
 cmsenv
+
+# clone genproductions repository
 echo "Cloning the genproductions repository..."
 git clone https://github.com/fabio-mon/genproductions --depth 1 --branch POWHEGggHH_cmssw106x
-cd genproductions
+
+# return to original working directory
+cd "$cwd"
