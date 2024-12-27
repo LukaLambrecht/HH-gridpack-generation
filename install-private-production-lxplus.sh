a7db10bce3cdd80e2968a7d5d3716677b165504e
LukaLambrecht <Luka.Lambrecht@UGent.be>
2024-12-27 15:47:03 +0100

    move sample production from gridpack to separate repo

--- install-private-production-lxplus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install the genproductions repository inside a suitable CMSSW release
-# Specific version for lxplus
-
-# Note: just a wrapper that calls the general installer
-# inside a cmssw-cc7 container.
-
-cmssw-cc7 --command-to-run 'bash ./install-private-production.sh'
