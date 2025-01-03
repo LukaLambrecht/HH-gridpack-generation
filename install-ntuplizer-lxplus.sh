64a5f39ab6888a7bbd4239cf172487582d6d5b89
LukaLambrecht <59565215+LukaLambrecht@users.noreply.github.com>
2025-01-03 21:04:26 +0100

    Delete unneeded file

--- install-ntuplizer-lxplus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install the ntuplizer repositories inside a suitable CMSSW release
-# Specific version for lxplus
-
-# Note: just a wrapper that calls the general installer
-# inside a cmssw-cc7 container.
-
-cmssw-cc7 --command-to-run 'bash ./install-ntuplizer.sh'
