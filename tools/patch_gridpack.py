52772137986ef86eb840eacbbea54edeb6dcbf97
LukaLambrecht <Luka.Lambrecht@UGent.be>
2025-01-10 21:43:19 +0100

    add patching of input file

--- tools/patch_gridpack.py
+++ tools/patch_gridpack.py
@@ -17,6 +17,8 @@ if __name__=='__main__':
       help='Path to the gridpack to patch')
     parser.add_argument('-o', '--output', default=None,
       help='Path for the patched output gridpack (default: overwrite input)')
+    parser.add_argument('--inputfile', default=None,
+      help='Alternative powheg.input file')
     parser.add_argument('--weightfile', default=None,
       help='Alternative scales and pdf weight file (to replace pwg-rwl.dat)')
     args = parser.parse_args()
@@ -34,7 +36,8 @@ if __name__=='__main__':
         if go!='y': sys.exit()
         args.output = args.gridpack
     else: args.output = os.path.abspath(args.output)
-    if args.weightfile is not None: args.weightfie = os.path.abspath(args.weightfile)
+    if args.inputfile is not None: args.inputfile = os.path.abspath(args.inputfile)
+    if args.weightfile is not None: args.weightfile = os.path.abspath(args.weightfile)
 
     # set path to this directory and other path-related variables
     # (for use later)
@@ -57,6 +60,12 @@ if __name__=='__main__':
     os.system(f'rm {tmpgridpack}')
 
     # --- start patching here ---
+
+    # replace weight file
+    if args.inputfile is not None:
+        print('Replacing input file...')
+        dest = os.path.join(tmpdir, 'powheg.input')
+        os.system(f'cp {args.inputfile} {dest}')
 
     # replace weight file
     if args.weightfile is not None:
